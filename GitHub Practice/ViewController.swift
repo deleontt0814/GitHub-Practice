2a0b110dc3d3e420c4fac9139c7b6e7e3b62597d
Timothy de Leon <timmydl0814@yahoo.com>
2018-06-12 17:44:49 -0700

    Initial Commit

---
+++ GitHub Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Practice
+//
+//  Created by Timothy De leon on 6/12/18.
+//  Copyright © 2018 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
